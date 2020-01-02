c5df5b99789337035e64c553c8ca8a11b4e15567
Joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-01 19:23:32 -0500

    updated project

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,10 @@
 import tweepy
 import logging
 from config import create_api
+from random import randint
+from datetime import datetime
 import time
+import os
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -17,8 +20,17 @@ def follow_followers(api):
 
 def main():
     api = create_api()
-    api.update_with_media("memes/meme_00.jpg")
-    while False:
+    imgs = os.listdir("memes")
+    while True:
+        date = datetime.now()
+        if not len(imgs):
+            logger.info("There aren't any images to tweet.")
+            exit(1)
+        if date.hour is 5 and date.minute is 30:
+                f = "memes/" + imgs[randint(0, len(imgs) - 1)]
+                api.update_with_media(f)
+                os.remove(f)
+                logger.info("Tweeted {}.".format(date.day))
         follow_followers(api)
         logger.info("Waiting...")
         time.sleep(60)
